b0c23c9178651c0dae083b67ade07b0eb8e14ada
daryanadoria <dariana.butenko.s@gmail.com>
2020-04-22 13:30:16 +0300

    js files

---
+++ module4/SpeakHello.js
@@ -0,0 +1,13 @@
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+// See Lecture 52, part 2
+
+(function (window) {
+var helloSpeaker = {};
+helloSpeaker.speakWord = "Hello";
+helloSpeaker.speak = function (name) {
+  console.log(helloSpeaker.speakWord + " " + name);
+}
+
+window.helloSpeaker = helloSpeaker;
+
+}) (window);
